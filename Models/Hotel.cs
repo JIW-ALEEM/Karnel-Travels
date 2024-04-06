2fe8c3d5755299ca456e3d03753debc0295906b6
Muhammad Aleem <m.aleem.aptech@gmail.com>
2024-04-06 11:09:31 +0500

    Validation in hotel

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Karnel_Travels.Models;
 
 public partial class Hotel
 {
     public int HotelId { get; set; }
-
+    [Required]
     public string HotelName { get; set; } = null!;
-
+    [Required]
     public string HotelImage { get; set; } = null!;
-
+    [Required]
     public string HotelDescription { get; set; } = null!;
-
+    [Required]
     public string HotelLocation { get; set; } = null!;
 
     public virtual ICollection<Package> Packages { get; } = new List<Package>();
